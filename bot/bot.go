22c33b248e89135e973e2111f22ec84ad1761c63
Gabriel Zhang <gabriel.b.zhang@gmail.com>
2024-06-10 16:41:03 +0800

    feat: add bubbletea ui

---
+++ bot/bot.go
@@ -0,0 +1,98 @@
+package bot
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	apiURL     = "https://api.openai.com/v1/chat/completions"
+	apiReqBody = `{
+        "model": "%s",
+        "messages": [
+            {
+                "role": "system",
+                "content": "Make sure all your responses are in Markdown format."
+            },
+            {
+                "role": "user",
+                "content": "%s"
+            }
+        ]
+    }`
+)
+
+func SetUp() {
+	viper.SetDefault("apiKey", "")
+	viper.SetDefault("model", "gpt-3.5-turbo")
+	viper.SetDefault("httpProxy", "")
+	viper.SetDefault("timeOut", 30)
+	viper.SetConfigName("gpterm")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			if err := viper.SafeWriteConfig(); err != nil {
+				panic(fmt.Errorf("fatal error config file: %w", err))
+			}
+			fmt.Println("No config file found. A default config file is created. Please set the apiKey and the httpProxy.")
+		} else {
+			panic(fmt.Errorf("fatal error config file: %w", err))
+		}
+	}
+}
+
+func Ask(input string) string {
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer([]byte(fmt.Sprintf(apiReqBody, viper.GetString("model"), strings.TrimSpace(input)))))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", viper.GetString("apiKey")))
+
+	proxyUrl, err := url.Parse(viper.GetString("httpProxy"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := (&http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+		Timeout: time.Duration(viper.GetInt("timeOut") * int(time.Second)),
+	}).Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, resBody)
+	}
+
+	var apiRes struct {
+		Choices [1]struct {
+			Message struct {
+				Content string
+			}
+		}
+	}
+	dec := json.NewDecoder(strings.NewReader(string(resBody)))
+	err = dec.Decode(&apiRes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return apiRes.Choices[0].Message.Content
+}
--- main.go
+++ main.go
@@ -1,110 +1,126 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
-	"os"
 	"strings"
-	"time"
 
+	"github.com/gabezbm/gpterm/bot"
+
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/glamour"
-	"github.com/spf13/viper"
+	"github.com/charmbracelet/lipgloss"
 )
 
-const (
-	apiURL     = "https://api.openai.com/v1/chat/completions"
-	apiReqBody = `{
-        "model": "%s",
-        "messages": [
-            {
-                "role": "system",
-                "content": "Make sure all your responses are in Markdown format."
-            },
-            {
-                "role": "user",
-                "content": "%s"
-            }
-        ]
-    }`
+var (
+	youStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("#ed8796")).Bold(true).Border(lipgloss.RoundedBorder(), true, true, true, true)
+	botStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("#7dc4e4")).Bold(true).Border(lipgloss.RoundedBorder(), true, true, true, true)
+	messageStyle = "dark"
 )
 
 func main() {
-	viper.SetDefault("apiKey", "")
-	viper.SetDefault("httpProxy", "")
-	viper.SetDefault("timeOut", 30)
-	viper.SetDefault("model", "gpt-4-turbo")
-	viper.SetConfigName("gpterm")
-	viper.SetConfigType("toml")
-	viper.AddConfigPath(".")
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			log.Println(viper.SafeWriteConfig())
-			fmt.Println("No config file found. A default config file is created. Please set the apiKey and the httpProxy.")
-		} else {
-			panic(fmt.Errorf("fatal error config file: %w", err))
-		}
-	}
-
-	apiKey := viper.GetString("apiKey")
-	httpProxy := viper.GetString("httpProxy")
-
-	input, _, err := bufio.NewReader(os.Stdin).ReadLine()
-	if err != nil {
+	bot.SetUp()
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
 
-	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer([]byte(fmt.Sprintf(apiReqBody, viper.GetString("model"), input))))
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", apiKey))
+type model struct {
+	viewport viewport.Model
+	textarea textarea.Model
+	messages []string
+}
 
-	proxyUrl, err := url.Parse(httpProxy)
-	if err != nil {
-		log.Fatal(err)
-	}
-	res, err := (&http.Client{
-		Transport: &http.Transport{
-			Proxy: http.ProxyURL(proxyUrl),
-		},
-        Timeout: time.Duration(viper.GetInt("timeOut") * int(time.Second)),
-	}).Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	resBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	res.Body.Close()
-	if res.StatusCode > 299 {
-		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, resBody)
+func initialModel() model {
+	vp := viewport.New(100, 35)
+	vp.SetContent(`Have a chat with Bot!
+Type a message and press Enter to send.`)
+	vp.KeyMap = viewport.KeyMap{
+		PageUp: key.NewBinding(
+			key.WithKeys("pgup"),
+			key.WithHelp("pgup", "page up"),
+		),
+		PageDown: key.NewBinding(
+			key.WithKeys("pgdown"),
+			key.WithHelp("pgdn", "page down"),
+		),
+		Up: key.NewBinding(
+			key.WithKeys("ctrl+up"),
+			key.WithHelp("ctrl+up", "up"),
+		),
+		Down: key.NewBinding(
+			key.WithKeys("ctrl+down"),
+			key.WithHelp("ctrl+down", "down"),
+		),
 	}
 
-	var apiRes struct {
-		Choices [1]struct {
-			Message struct {
-				Content string
+	ta := textarea.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus()
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+	ta.Prompt = "┃ "
+	ta.ShowLineNumbers = false
+	ta.SetWidth(100)
+	ta.SetHeight(5)
+	ta.CharLimit = 1000
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		viewport: vp,
+		textarea: ta,
+		messages: []string{},
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		vpCmd tea.Cmd
+		taCmd tea.Cmd
+	)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+	m.textarea, taCmd = m.textarea.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			input := m.textarea.Value()
+			m.textarea.Reset()
+			renderedOutput, err := glamour.Render(input, messageStyle)
+			if err != nil {
+				log.Fatal(err)
 			}
+			m.messages = append(m.messages, youStyle.Render("You: ")+renderedOutput)
+			renderedOutput, err = glamour.Render(bot.Ask(input), messageStyle)
+			if err != nil {
+				log.Fatal(err)
+			}
+			m.messages = append(m.messages, botStyle.Render("Bot: ")+renderedOutput)
+			m.viewport.SetContent(strings.Join(m.messages, "\n"))
+			m.viewport.GotoBottom()
 		}
-	}
-	dec := json.NewDecoder(strings.NewReader(string(resBody)))
-	err = dec.Decode(&apiRes)
-	if err != nil {
-		log.Fatal(err)
+	case error:
+		log.Fatal(msg)
+		return m, nil
 	}
 
-	renderedOutput, err := glamour.Render(apiRes.Choices[0].Message.Content, "dark")
-	if err != nil {
-		log.Fatal(err)
-	}
+	return m, tea.Batch(vpCmd, taCmd)
+}
 
-	fmt.Printf("%s", renderedOutput)
+func (m model) View() string {
+	return fmt.Sprintf(
+		"\n\n%s\n\n%s",
+		m.viewport.View(),
+		m.textarea.View(),
+	) + "\n\n"
 }
